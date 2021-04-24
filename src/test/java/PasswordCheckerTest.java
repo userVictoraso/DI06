43633a0087412f9d4e3eea960cf9ad72238db9c0
userVictoraso <al.victor.pareja.ramirez@iesportada.org>
2021-04-24 15:05:51 +0200

    queda poco

--- src/test/java/PasswordCheckerTest.java
+++ src/test/java/PasswordCheckerTest.java
@@ -1,10 +1,8 @@
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/** @AUTHOR Victor Pareja Ramírez **/
 
 public class PasswordCheckerTest {
     private final PasswordChecker passwordChecker = new PasswordChecker();
@@ -32,18 +30,22 @@ public class PasswordCheckerTest {
     /**WEAK PASSWORD**/
     /** Checking weak password with strings less than 4 chars**/
     @Test
+    @DisplayName("Checking weak password with strings less than 4 chars")
     public void weakPasswordLessThan4Chars() {
         boolean actual = passwordChecker.checkWeakPassword("Vic");
         assertFalse(actual);
     }
     /**Checking weak password with strings equals to 4 chars**/
     @Test
+    @DisplayName("Checking weak password with strings equals to 4 chars")
     public void weakPasswordEqualsTo4Chars() {
         boolean actual = passwordChecker.checkWeakPassword("Vict");
         assertTrue(actual);
+        fail();
     }
     /**Checking weak password with strings greater than 4 chars**/
     @Test
+    @DisplayName("Checking weak password with strings greater than 4 chars")
     public void weakPasswordGreaterThan4Chars() {
         boolean actual = passwordChecker.checkWeakPassword("Victor");
         assertTrue(actual);
@@ -52,6 +54,7 @@ public class PasswordCheckerTest {
     /**MEDIUM PASSWORD**/
     /**Checking medium password with strings less than 5 chars**/
     @Test
+    @DisplayName("Checking medium password with strings less than 5 chars")
     public void mediumPasswordLessThan5Chars() {
         boolean actual = passwordChecker.checkMediumPassword("Vict");
         assertFalse(actual);
@@ -59,6 +62,7 @@ public class PasswordCheckerTest {
 
     /**Checking medium password with strings equals to 5 chars**/
     @Test
+    @DisplayName("Checking medium password with strings equals to 5 chars")
     public void mediumPasswordEqualsTo5Chars() {
         boolean actual = passwordChecker.checkWeakPassword("Victo");
         assertTrue(actual);
@@ -66,6 +70,7 @@ public class PasswordCheckerTest {
 
     /**Checking medium password with strings greater than 5 chars**/
     @Test
+    @DisplayName("Checking medium password with strings greater than 5 chars")
     public void mediumPasswordGreaterThan5Chars() {
         boolean actual = passwordChecker.checkWeakPassword("Victor");
         assertTrue(actual);
@@ -74,6 +79,7 @@ public class PasswordCheckerTest {
     /**STRONG PASSWORD**/
     /**Checking strong password with strings less than 7 chars**/
     @Test
+    @DisplayName("Checking strong password with strings less than 7 chars")
     public void strongPasswordLessThan7Chars() {
         boolean actual = passwordChecker.checkStrongPassword("Victor");
         assertFalse(actual);
@@ -81,24 +87,28 @@ public class PasswordCheckerTest {
 
     /**Checking strong password with strings equals to 7 chars, only letter**/
     @Test
+    @DisplayName("Checking strong password with strings equals to 7 chars, only letter")
     public void strongPasswordEqualsTo7CharsOnlyLetter() {
         boolean actual = passwordChecker.checkStrongPassword("Victora");
         assertFalse(actual);
     }
     /**Checking strong password with strings greater than 7 chars, only letter**/
     @Test
+    @DisplayName("Checking strong password with strings greater than 7 chars, only letter")
     public void strongPasswordGreaterThan7CharsOnlyLetter() {
         boolean actual = passwordChecker.checkStrongPassword("Victoraso");
         assertFalse(actual);
     }
     /**Checking strong password with strings equals to 7 chars, with number and lowercase**/
     @Test
+    @DisplayName("Checking strong password with strings equals to 7 chars, with number and lowercase")
     public void strongPasswordEqualsTo7CharsNumberLowercase() {
         boolean actual = passwordChecker.checkStrongPassword("victor7");
         assertFalse(actual);
     }
     /**Checking strong password with strings equals to 7 chars, with number, lowercase and uppercase**/
     @Test
+    @DisplayName("Checking strong password with strings equals to 7 chars, with number, lowercase and uppercase")
     public void strongPasswordEqualsTo7CharsNumberLowercaseUppercase() {
         boolean actual = passwordChecker.checkStrongPassword("Victor7");
         assertTrue(actual);
